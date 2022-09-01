17838f359531e162ae27a262d86d2a64939a1301
bgok <ken@citizenhodler.com>
2022-09-01 06:56:14 -0700

    Tidy things up

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import {useNearAccount, useNearUser} from "react-near";
 import {DEFAULT_CONNECTION_CONTRACT} from "./config";
-import {useHasNft} from "./hooks/useHasNft"; /* works */
-// import {useHasNft} from "near-access-react"; /* React throws a hooks error */
+import {useHasNft} from "near-access-react/dist";
 
 function App() {
     const account = useNearAccount()
